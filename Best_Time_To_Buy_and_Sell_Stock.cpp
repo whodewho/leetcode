dc862dfb9f7d4d06b3914cd6cca254347a71aa03
Kai Chen <chenkainp@gmail.com>
2013-07-12 16:53:04 +0800

    one more

--- Best_Time_To_Buy_and_Sell_Stock.cpp
+++ Best_Time_To_Buy_and_Sell_Stock.cpp
@@ -1,34 +1,16 @@
-class Solution {
+lass Solution {
     public:
         int maxProfit(vector<int> &prices) {
             // Start typing your C/C++ solution below
             // DO NOT write int main() function
-            if(prices.empty())
-            {
-                return 0;
-            }
-            int minR,maxR,minT,maxT;
-            minR=maxR=minT=maxT=prices[0];
+            int res=0;
+            int min_i=0;
             for(int i=1;i<prices.size();i++)
             {
-                if(prices[i]>maxT)
-                {
-                    maxT=prices[i];
-                    if(maxT>maxR)
-                    {
-                        maxR=maxT;
-                    }
-                    if(maxT-minT>maxR-minR)
-                    {
-                        maxR=maxT;
-                        minR=minT;
-                    }
-                }
-                if(prices[i]<minT)
-                {
-                    minT=maxT=prices[i];
-                }
+                if(prices[i]<prices[min_i])min_i=i;
+                int diff=prices[i]-prices[min_i];
+                if(diff>res)res=diff;
             }
-            return maxR-minR;
+            return res;
         }
 };
