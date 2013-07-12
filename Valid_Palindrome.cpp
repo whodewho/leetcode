b716952695b30dd37e6e91fe64a80b9fe28d1110
Kai Chen <chenkainp@gmail.com>
2013-07-12 16:28:52 +0800

    one more

---
+++ Valid_Palindrome.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    public:
+        bool isPalindrome(string s) {
+            // Start typing your C/C++ solution below
+            // DO NOT write int main() function
+            int i=0,j=s.length()-1;
+            while(i<j)
+            {
+                if(!isalnum(s[i]))
+                {
+                    i++;
+                    continue;
+                }
+                else if(!isalnum(s[j]))
+                {
+                    j--;
+                    continue;
+                }
+                else if(tolower(s[i])!=tolower(s[j]))
+                {
+                    return false;
+                }
+                i++;
+                j--;
+            }
+            return true;
+        }
+};
