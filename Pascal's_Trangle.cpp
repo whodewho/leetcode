a854291d64e5eb3a0329ba89315cc393a18ad58e
Kai Chen <chenkainp@gmail.com>
2013-07-13 15:49:10 +0800

    one more

---
+++ Pascal's_Trangle.cpp
@@ -0,0 +1,26 @@
+/*
+   is the reserve function improve the solution? I tried, not sure.
+   */
+class Solution {
+    public:
+        vector<vector<int> > generate(int numRows) {
+            // Start typing your C/C++ solution below
+            // DO NOT write int main() function
+            vector<vector<int>> res;
+            if(numRows==0)return res;
+            res.push_back(vector<int>(1,1));
+            for(int i=1;i<numRows;i++)
+            {
+                vector<int> item;
+                //item.reserve(i+1);
+                item.push_back(1);
+                for(int j=1;j<i;j++)
+                {
+                    item.push_back(res[i-1][j]+res[i-1][j-1]);
+                }
+                item.push_back(1);
+                res.push_back(item);
+            }
+            return res;
+        }
+};
