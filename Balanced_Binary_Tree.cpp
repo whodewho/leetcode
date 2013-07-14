10b3fb1ce4b435909b4bcb98c471a6fc6fd8f70c
Kai Chen <chenkainp@gmail.com>
2013-07-14 20:54:02 +0800

    one more

---
+++ Balanced_Binary_Tree.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+/*
+    mine is better.I just love referrence.
+    if left subtree is not balanced, just return, froget right subtree.
+   */
+class Solution {
+    public:
+        bool isBalanced(TreeNode *root) {
+            // Start typing your C/C++ solution below
+            // DO NOT write int main() function
+            int depth=0;
+            return worker(root,depth);
+        }
+
+        bool worker(TreeNode *root,int &depth)
+        {
+            if(root==NULL)
+            {
+                return true;
+            }
+            int ld=0,rd=0;
+            if(!worker(root->left,ld))return false;
+            if(!worker(root->right,rd))return false;
+
+            depth=max(ld,rd)+1;
+            return abs(ld-rd)<=1;
+        }
+};
