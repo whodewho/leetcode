6f3dabc32501baf56f15b12fec5cf2f3d3577be9
Kai Chen <chenkainp@gmail.com>
2013-07-14 21:13:52 +0800

    one more

---
+++ Convert_Sorted_Array_to_Binary_Search_Tree.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+/*
+    array, that's easy. the middle of the array is the root node.
+   */
+class Solution {
+    public:
+        TreeNode *sortedArrayToBST(vector<int> &num) {
+            // Start typing your C/C++ solution below
+            // DO NOT write int main() function
+            return worker(num,0,num.size()-1);
+        }
+        TreeNode * worker(vector<int> &num,int low,int high)
+        {
+            if(low>high)return NULL;
+            int mid=(low+high)/2;
+            TreeNode *root=new TreeNode(num[mid]);
+            root->left=worker(num,low,mid-1);
+            root->right=worker(num,mid+1,high);
+            return root;
+        }
+};
