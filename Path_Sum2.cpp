d0787e75a39eee6c1da204e29f51c440f1f6b619
Kai Chen <chenkainp@gmail.com>
2013-07-14 16:12:05 +0800

    one more

---
+++ Path_Sum2.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+/*
+    could this be more succient? just stop recursion at if(root==NULL)?
+    it won't work. dup path will be push_backed in res.
+   */
+class Solution {
+    public:
+        vector<vector<int> > pathSum(TreeNode *root, int sum) {
+            // Start typing your C/C++ solution below
+            // DO NOT write int main() function
+            vector<vector<int> > res;
+            if(root==NULL)return res;
+            vector<int> path;
+            worker(root,sum,res,path);
+            return res;
+        }
+        void worker(TreeNode *root,int sum,vector<vector<int>> &res,vector<int> path)
+        {
+            if(root->left==NULL&&root->right==NULL)
+            {
+                if(sum-root->val==0)
+                {
+                    path.push_back(root->val);
+                    res.push_back(path);
+                }
+                return;
+            }
+            path.push_back(root->val);
+            if(root->left)worker(root->left,sum-root->val,res,path);
+            if(root->right)worker(root->right,sum-root->val,res,path);
+            path.pop_back();        
+        }
+};
