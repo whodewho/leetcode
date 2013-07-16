f487512f17b68da48b013e072af5bdd78acae4dd
Kai Chen <chenkainp@gmail.com>
2013-07-16 22:51:15 +0800

    one more

---
+++ Binary_Tree_Level_Order_Traversal2.cpp
@@ -0,0 +1,72 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+/*
+   use stack to store the level nodes is also a good choice.
+   anson, i guess you like. although, a reverse at last can also sovle the problem.
+   stack is not moe efficient than reverse, in my opinion.
+   the second one is anson's solution, twice the space it costs.
+ */
+class Solution {
+    public:
+        vector<vector<int> > levelOrderBottom(TreeNode *root) {
+            // Start typing your C/C++ solution below
+            // DO NOT write int main() function
+            vector<vector<int> > res;
+            vector<int> row;
+            queue<TreeNode*> currQ;
+            int currNum = 1, nextNum = 0;
+            if (root) currQ.push(root);
+            while (!currQ.empty()) {
+                TreeNode* front = currQ.front();
+                currQ.pop(), currNum--;
+                row.push_back(front->val);
+                if (front->left) currQ.push(front->left), nextNum++;
+                if (front->right) currQ.push(front->right), nextNum++;
+                if (currNum == 0) {
+                    res.push_back(row);
+                    row.clear();
+                    currNum = nextNum;
+                    nextNum = 0;
+                }
+            }
+            reverse(res.begin(),res.end());
+            return res;
+        }
+
+        vector<vector<int> > levelOrderBottom2(TreeNode *root)
+        {
+            vector<vector<int> > res;
+            if (!root) return res;
+
+            stack<vector<TreeNode*> > stk;
+            stk.push(vector<TreeNode*>(1, root));
+            while(true)
+            {
+                vector<TreeNode*> row;
+                for (vector<TreeNode*>::iterator it = stk.top().begin(); it != stk.top().end(); ++it)
+                {
+                    if ((*it)->left) row.push_back((*it)->left);
+                    if ((*it)->right) row.push_back((*it)->right);
+                }
+                if (row.empty()) break;
+                stk.push(row);
+            }
+
+            while (!stk.empty())
+            {
+                vector<int> row;
+                for (vector<TreeNode*>::iterator it = stk.top().begin(); it != stk.top().end(); ++it)
+                    row.push_back((*it)->val);
+                res.push_back(row);
+                stk.pop();
+            }
+            return res;
+        }
+};
