6a06ab94a4d25c050fc2c25aaa667dd6545d92ad
Kai Chen <chenkainp@gmail.com>
2013-07-28 13:41:26 +0800

    one more

---
+++ Remove_Duplicates_from_Sorted_Array2.cpp
@@ -0,0 +1,30 @@
+class Solution {
+    public:
+        int removeDuplicates(int A[], int n) {
+            // Start typing your C/C++ solution below
+            // DO NOT write int main() function
+            if(n<=1)return n;
+            int count=0,k=0;
+            for(int i=1;i<n;i++)
+            {
+                if(A[i]==A[k])
+                {
+                    if(count<1)
+                    {
+                        count++;
+                        A[++k]=A[i];
+                    }
+                }
+                else
+                {
+                    A[++k]=A[i];
+                    count=0;
+                }
+            }
+            for(int j=k+1;j<n;j++)
+            {
+                A[j]='\0';
+            }
+            return k+1;
+        }
+};
