4a261cd178d24960e7080cd081d30d69514314e0
Kai Chen <chenkainp@gmail.com>
2013-07-28 14:16:07 +0800

    one more

---
+++ Remove_Duplicates_from_Sorted_List.cpp
@@ -0,0 +1,19 @@
+/*
+    better
+   */
+class Solution {
+    public:
+        ListNode *deleteDuplicates(ListNode *head) {
+            if (NULL == head) return NULL;
+            ListNode* curNode = head;
+            while(NULL != curNode->next) {
+                if(curNode->val == curNode->next->val) {
+                    ListNode* nextNode = curNode->next;
+                    curNode->next = nextNode->next;
+                    delete nextNode;
+                }
+                else curNode = curNode->next;
+            }
+            return head;
+        }
+};
