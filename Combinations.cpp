b19c9f91267d7919e696aac23205b0c4f801b317
Kai Chen <chenkainp@gmail.com>
2013-07-28 15:19:16 +0800

    one more

---
+++ Combinations.cpp
@@ -0,0 +1,28 @@
+/*
+    almost same with anson's
+   */
+class Solution {
+    public:
+        vector<vector<int> > combine(int n, int k) {
+            // Start typing your C/C++ solution below
+            // DO NOT write int main() function
+            vector<vector<int >> res;
+            if(k>n)return res;
+
+            vector<int> item;
+            worker(1,n,k,res,item);
+            return res;
+        }
+
+        void worker(int start,int n,int k,vector<vector<int>> &res,vector<int> &item)
+        {
+            if(k==0){res.push_back(item);return;}
+            if(k>n-start+1)return;
+
+            item.push_back(start);
+            worker(start+1,n,k-1,res,item);
+            item.pop_back();
+
+            worker(start+1,n,k,res,item);
+        }
+};
