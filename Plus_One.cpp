0e77ba7b9bca72eade81ebe5adffb6309a8d13b8
Kai Chen <chenkainp@gmail.com>
2013-08-28 11:38:16 +0800

    one more

---
+++ Plus_One.cpp
@@ -0,0 +1,18 @@
+class Solution {
+    public:
+        vector<int> plusOne(vector<int> &digits) {
+            // Start typing your C/C++ solution below
+            // DO NOT write int main() function
+            int carry=1;
+            vector<int> res;
+            for(int i=digits.size()-1;i>=0;i--)
+            {
+                res.push_back((digits[i]+carry)%10);
+                carry=(digits[i]+carry)/10;  
+            }
+            if(carry)
+                res.push_back(carry);
+            reverse(res.begin(),res.end());
+            return res;
+        }
+};
