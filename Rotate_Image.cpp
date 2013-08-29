cc33320708d181c6b838d449ae1162dd5b5d2aae
Kai Chen <chenkainp@gmail.com>
2013-08-29 20:50:34 +0800

    one more

---
+++ Rotate_Image.cpp
@@ -0,0 +1,28 @@
+/*
+    be careful about the (i,j), the length.
+   */
+class Solution {
+    public:
+        void rotate(vector<vector<int> > &matrix) {
+            // Start typing your C/C++ solution below
+            // DO NOT write int main() function
+            int m = matrix.size();
+            if (!m)
+                return;
+            int n = matrix[0].size();
+            if (!n)
+                return;
+
+            for (int i = 0; i < m / 2; i++) {
+                int l = n - 2 * i;
+                for (int j = i; j < i + l - 1; j++) {
+                    int key = matrix[i][j];
+                    matrix[i][j] = matrix[m - j - 1][i];
+                    matrix[m - j - 1][i] = matrix[m - i - 1][n - j - 1];
+                    matrix[m - i - 1][n - j - 1] = matrix[j][n - i - 1];
+                    matrix[j][n - i - 1] = key;
+                }
+            }
+
+        }
+};
