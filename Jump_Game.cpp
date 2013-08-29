5cca6db146d6be8b4a25198a51fa9992d6934f51
Kai Chen <chenkainp@gmail.com>
2013-08-29 14:58:25 +0800

    one more

---
+++ Jump_Game.cpp
@@ -0,0 +1,18 @@
+/*
+    anson, clean and tidy
+   */
+class Solution {
+    public:
+        bool canJump(int A[], int n) {
+            // Start typing your C/C++ solution below
+            // DO NOT write int main() function
+            int i = 0;
+            int next = 0;
+            while (i <= next) {
+                next = max(next, i + A[i]);
+                if (next >= n - 1) return true;
+                i++;
+            }
+            return false;
+        }
+};
