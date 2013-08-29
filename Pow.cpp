69ad20491a3bbb06d982d8b47b3d24fda2ea8a8f
Kai Chen <chenkainp@gmail.com>
2013-08-29 16:05:27 +0800

    one more

---
+++ Pow.cpp
@@ -0,0 +1,25 @@
+/*
+    nothing special, anson, yours helper is not helpful at all
+   */
+class Solution {
+    public:
+
+        double pow(double x, int n) {
+            // Start typing your C/C++ solution below
+            // DO NOT write int main() function
+            bool sign=false;
+            if(n==0)return 1;
+            if(n==1)return x;
+            if(n<0){
+                n=-n;
+                sign=true;   
+            }
+            double tmp=pow(x,n/2);
+            double result;
+
+            if(n%2)result=tmp*tmp*x;
+            else result=tmp*tmp;
+            if(sign)return 1/result;
+            return result;
+        }
+};
