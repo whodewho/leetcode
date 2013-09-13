36054089ee8aa322103fc215776e33e4f64d5eb7
Kai Chen <chenkainp@gmail.com>
2013-09-13 16:02:44 +0800

    second try

--- Two_Sum.cpp
+++ Two_Sum.cpp
@@ -29,4 +29,45 @@ class Solution {
             sort(result.begin(),result.end());
             return result;
         }
+
+        //second try
+
+        bool compare(pair<int, int> comp1, pair<int, int> comp2) {
+            return comp1.first < comp2.first;
+        }
+
+        struct myclass {
+            bool operator()(pair<int, int> comp1, pair<int, int> comp2) {
+                return comp1.first < comp2.first;
+            }
+        } myobject;
+
+        vector<int> twoSum(vector<int> &numbers, int target) {
+            // Start typing your C/C++ solution below
+            // DO NOT write int main() function
+            vector<int> result;
+            vector<pair<int, int> > numbersWithIndex;
+            for (size_t i = 0; i != numbers.size(); i++) {
+                numbersWithIndex.push_back(make_pair(numbers[i], i + 1));
+            }
+            std::sort(numbersWithIndex.begin(), numbersWithIndex.end(), compare);
+            size_t i = 0, j = numbers.size() - 1;
+            while (i <= j) {
+                int tmpSum = numbersWithIndex[i].first + numbersWithIndex[j].first;
+                if (tmpSum < target)
+                    i++;
+                else if (tmpSum > target)
+                    j--;
+                else {
+                    result.push_back(
+                            min(numbersWithIndex[i].second,
+                                numbersWithIndex[j].second));
+                    result.push_back(
+                            max(numbersWithIndex[i].second,
+                                numbersWithIndex[j].second));
+                    break;
+                }
+            }
+            return result;
+        }
 };
