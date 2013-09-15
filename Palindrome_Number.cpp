ef4596e804b338e50d0daa3a110774537222560f
Kai Chen <chenkainp@gmail.com>
2013-09-15 16:18:00 +0800

    one more

---
+++ Palindrome_Number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+    public:
+        bool isPalindrome(int x) {
+            if (x < 0) return false;
+            int d = 1;
+            while (x / d >= 10) d *= 10;
+            while (x != 0) {
+                int l = x / d;
+                int r = x % 10;
+                if (l != r) return false;
+                x = (x % d) / 10;
+                d /= 100;
+            }
+            return true;
+        }
+};
