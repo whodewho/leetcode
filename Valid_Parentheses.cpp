4a7131ce0af92c54c0db6de3216d0df4dde821dc
Kai Chen <chenkainp@gmail.com>
2013-09-16 15:59:41 +0800

    one more

---
+++ Valid_Parentheses.cpp
@@ -0,0 +1,28 @@
+/*
+    0 milli secs on large data, I am on fire!
+   */
+class Solution {
+    public:
+        bool isValid(string s) {
+            // Start typing your C/C++ solution below
+            // DO NOT write int main() function
+            stack<char> stk;
+            for(string::size_type i=0;i<s.size();i++)
+            {
+                if(stk.empty())
+                {
+                    stk.push(s[i]);
+                    continue;
+                }
+                if(s[i]==')'&&stk.top()=='('||s[i]=='}'&&stk.top()=='{'||s[i]==']'&&stk.top()=='[')
+                {
+                    stk.pop();
+                }
+                else
+                {
+                    stk.push(s[i]);
+                }
+            }
+            return stk.empty();
+        }
+};
