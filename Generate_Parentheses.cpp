80f271d6c5fcaee46d8d7cb66a17f3f502c98e76
Kai Chen <chenkainp@gmail.com>
2013-09-16 16:23:18 +0800

    one more

---
+++ Generate_Parentheses.cpp
@@ -0,0 +1,30 @@
+class Solution {
+    public:
+
+        void worker(int left,int right,int n,string path,vector<string> &result)
+        {
+            if(right==n)
+            {
+                result.push_back(path);
+                return;
+            }
+
+            if(left<n)
+            {
+                worker(left+1,right,n,path+"(",result);
+            }
+
+            if(left>right)
+            {
+                worker(left,right+1,n,path+")",result);
+            }
+        }
+
+        vector<string> generateParenthesis(int n) {
+            // Start typing your C/C++ solution below
+            // DO NOT write int main() function
+            vector<string> result;
+            worker(0,0,n,"",result);
+            return result;
+        }
+};
