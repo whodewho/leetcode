29798ed665f07e09217e8f744e2d34daee3cdf7d
Kai Chen <chenkainp@gmail.com>
2013-09-17 22:16:36 +0800

    one more

---
+++ Search_Insert_Position.cpp
@@ -0,0 +1,27 @@
+class Solution {
+    public:
+        int searchInsert(int A[], int n, int target) {
+            // Start typing your C/C++ solution below
+            // DO NOT write int main() function
+            int l=0,h=n-1;
+            while(l<=h)
+            {
+                int mid=(l+h)/2;
+                if(A[mid]<target)
+                {
+                    l=mid+1;
+                }
+                else if(A[mid]>target)
+                {
+                    h=mid-1;
+                }
+                else
+                {
+                    return mid;
+                }
+            }
+
+            if(target<A[l])return l;
+            else return h+1;
+        }
+};
