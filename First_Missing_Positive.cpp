1896598e1aa53ba8f891edd0b63a4b8431d63b1e
Kai Chen <chenkainp@gmail.com>
2013-09-18 20:49:59 +0800

    one more

--- First_Missing_Positive.cpp
+++ First_Missing_Positive.cpp
@@ -1,6 +1,6 @@
 /*
-    from anson, great. index and value, return index, who knows.
-   */
+   from anson, great. index and value, return index, who knows.
+ */
 int firstMissingPositive(int A[], int n) {
     int i = 0;
     while (i < n) {
@@ -13,4 +13,35 @@ int firstMissingPositive(int A[], int n) {
         if (A[i] != i + 1)
             break;
     return i + 1;
+}
+
+// second try, almost same, not that better
+int firstMissingPositive(int A[], int n) {
+    // Start typing your C/C++ solution below
+    // DO NOT write int main() function
+
+    for(int i=0;i<n;i++)
+    {
+        if(A[i]<=0||A[i]>n||A[i]-1==i||A[A[i]-1]==A[i])continue;
+        else
+        {
+            int tmp=A[A[i]-1];
+            A[A[i]-1]=A[i];
+            A[i]=tmp;
+            i--;
+        }
+    }
+    int i=1;
+    for(int j=0;j<n;j++)
+    {
+        if(A[j]==i)
+        {
+            i++;
+        }
+        else
+        {
+            return j+1;
+        }
+    }
+    return i;
 }
