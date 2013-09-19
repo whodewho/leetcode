f8677606b4456726af3ac293ce824735efe2c156
Kai Chen <chenkainp@gmail.com>
2013-09-19 22:22:55 +0800

    one more

--- Wildcard_Matching.cpp
+++ Wildcard_Matching.cpp
@@ -1,11 +1,15 @@
 /*
-    the first, time limit exceed
-    the second, same as the first
-    the third, anson's code, perfect
-    what's the difference with regression_match below?
-    here isMatch("aab","c*a*b") false
-    below true, the "*" is different
-   */
+   the first, time limit exceed, the second, same as the first
+   the third, anson's code, good, but, the code is hard to read
+   code is wirtten to read, sometimes, we run it
+   I will never read code 2 again, that's why
+   good code are beautiful code, always
+   the last one, from http://discuss.leetcode.com/questions/222/wildcard-matching
+   perfect!
+   what's the difference with regression_match below?
+   here isMatch("aab","c*a*b") false
+   below true, the "*" is different
+ */
 class Solution {
     public:
         bool isMatch(const char* s, const char* p) {
@@ -139,4 +143,43 @@ starCheck: if (!star)
            s++;
            goto loopStart;
            return false;
-        }}
+        }
+
+        bool isMatch(const char *s, const char *p) {
+            // Start typing your C/C++ solution below
+            // DO NOT write int main() function
+            if(!s && !p) return true;
+
+            const char *star_p=NULL,*star_s=NULL;
+
+            while(*s)
+            {
+                if(*p == '?' || *p == *s)
+                {
+                    ++p,++s;
+                }else if(*p == '*')
+                {
+                    //skip all continuous '*'
+                    while(*p == '*') ++p;
+
+                    if(!*p) return true; //if end with '*', its match.
+
+                    star_p = p; //store '*' pos for string and pattern
+                    star_s = s;
+                }else if((!*p || *p != *s)  && star_p)
+                {
+                    s = ++star_s; //skip non-match char of string, regard it matched in '*'
+                    p = star_p; //pattern backtrace to later char of '*'
+                }else
+                    return false;
+            }
+
+            //check if later part of p are all '*'
+            while(*p)
+                if(*p++ != '*')
+                    return false;
+
+            return true;
+        }
+
+}
