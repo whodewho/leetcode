b0c9961face98d09abb3550c7fcdac37e9247ca8
Kai Chen <chenkainp@gmail.com>
2013-09-20 11:42:15 +0800

    one more

--- N-Queens2.cpp
+++ N-Queens2.cpp
@@ -1,7 +1,7 @@
 /*
-    does not make too much sense
-    haha, anson is learning
-   */
+   does not make too much sense
+   haha, anson is learning
+ */
 class Solution {
     public:
         int totalNQueens(int n) {
@@ -36,4 +36,37 @@ class Solution {
             }
         }
 
+        //iterative way
+        #define N 10 
+        int x[N] = { 0 };
+
+        int place(int k) //判断是否符合要求；
+        {
+            int j;
+            for (j = 0; j < k; j++) {
+                if ((abs(k - j) == abs(x[j] - x[k])) || (x[j] == x[k]))
+                    return 0;
+            }
+            return 1;
+        }
+        void queen(int &count) {
+            int i, k;
+            x[0] = -1;
+            k = 0;
+            while (k >= 0) {
+                x[k] += 1;
+                while ((x[k] < N) && !(place(k)))
+                    x[k] += 1;
+                if (x[k] < N) {
+                    if (k == N - 1)
+                        count++; //k=N-1 表示也可构成一个排法；
+                    else {
+                        k++;
+                    }
+                } else {
+                    x[k] = -1; //when go back, recover the enviroment
+                    k--;
+                }
+            }
+        }
 };
