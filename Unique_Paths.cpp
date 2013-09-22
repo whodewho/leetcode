a6c334d0005c2a5b1a13a8886f2a09446ac590f5
Kai Chen <chenkainp@gmail.com>
2013-09-22 19:53:36 +0800

    one more

--- Unique_Paths.cpp
+++ Unique_Paths.cpp
@@ -1,6 +1,7 @@
 /*
-    anson is changing
-   */
+   anson is changing
+   look at my second try
+ */
 class Solution {
     public:
         int uniquePaths(int m, int n) {
@@ -20,5 +21,16 @@ class Solution {
                     else p[i][j]=p[i+1][j]+p[i][j+1];
                 }
             return p[0][0];
+        }
+
+        int uniquePaths(int m, int n) {
+            // Start typing your C/C++ solution below
+            // DO NOT write int main() function
+            if(m==0||n==0)return 0;
+            vector<vector<int> > f(m,vector<int>(n,1));
+            for(int i=m-2;i>=0;i--)
+                for(int j=n-2;j>=0;j--)
+                    f[i][j]=f[i+1][j]+f[i][j+1];
+            return f[0][0];
         }
 };
