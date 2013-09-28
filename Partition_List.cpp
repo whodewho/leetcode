30e030e0bad1d2d8c1cef3cfb4b6e613585c33f4
Kai Chen <chenkainp@gmail.com>
2013-09-28 22:11:13 +0800

    second try

--- Partition_List.cpp
+++ Partition_List.cpp
@@ -7,9 +7,9 @@
  * };
  */
 /*
-    have checked anson's, his is clear, mine is faster.
-    when curr->val<x, no need to prev->next=curr
-   */
+   have checked anson's, his is clear, mine is faster.
+   when curr->val<x, no need to prev->next=curr
+ */
 class Solution {
     public:
         ListNode *partition(ListNode *head, int x) {
@@ -55,5 +55,54 @@ class Solution {
                 head=newHead;
             }
             return head;
+        }
+
+
+        ListNode *partition(ListNode *head, int x) {
+            // Start typing your C/C++ solution below
+            // DO NOT write int main() function
+            if(!head)return head;
+            ListNode* less=NULL;
+            ListNode* lessRunner=NULL;
+            ListNode* great=NULL;
+            ListNode* greatRunner=NULL;
+            while(head)
+            {
+                if(head->val<x)
+                {
+                    if(!less)
+                    {
+                        less=head;
+                    }
+                    else
+                    {
+                        lessRunner->next=head;
+                    }
+                    lessRunner=head;
+                    head=head->next;
+                    lessRunner->next=NULL;
+                }
+                else
+                {
+                    if(!great)
+                    {
+                        great=head;
+                    }
+                    else
+                    {
+                        greatRunner->next=head;
+                    }
+                    greatRunner=head;
+                    head=head->next;
+                    greatRunner->next=NULL;
+                }
+            }
+
+            if(less)
+            {
+                lessRunner->next=great;
+                return less;
+            }
+            return great;
         }
 };
