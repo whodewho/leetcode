5f56107bd6c4e8d2dad0dcf34a9ffa21ae26e720
Kai Chen <chenkainp@gmail.com>
2013-09-28 23:07:21 +0800

    second try

--- Regular_Expression_Matching.cpp
+++ Regular_Expression_Matching.cpp
@@ -1,9 +1,11 @@
 //http://blog.csdn.net/hopeztm/article/details/7992253
 /*
-    c* means "" or "cccc..."
-    not "c" or "cccc...." 
-    * match 0 or more preceding...I didn't get it until one minute ago
-   */
+   c* means "" or "cccc..."
+   not "c" or "cccc...." 
+   match 0 or more preceding...I didn't get it until one minute ago
+
+   every recursion way has a iteration alteration
+ */
 class Solution {
     public:
         bool isMatch(const char *s, const char *p) {
@@ -31,8 +33,62 @@ class Solution {
                     s++;
                 }
                 return isMatch(s, p + 2);
-
             }
+        }
 
+
+        bool isMatch(const char *s, const char *p) {
+            // Start typing your C/C++ solution below
+            // DO NOT write int main() function
+            if (!s || !p)
+                return false;
+
+            int slen = strlen(s);
+            int plen = strlen(p);
+            vector<bool> dp[plen + 1];
+
+            for (int i = 0; i <= plen; i++)
+                dp[i] = vector<bool>(slen + 1, false);
+
+            dp[0][0] = true;
+            for (int i = 1; i <= plen; i++) {
+                switch (p[i - 1]) {
+                    case '*': {
+                                  dp[i][0] = i > 1 ? dp[i - 2][0] : false;
+                                  if (i < 2)
+                                      continue;
+                                  if (p[i - 2] != '.') {
+                                      //*号不用,dp[i-1][j]
+                                      //'p[i-2]*'不用,i>=2 && dp[i-2][j]
+                                      //'p[i-2]*'用上,j>=2dp[i][j-1]&&s[j-1]==s[j-2]&&s[j-2]==p[j-2], so complicated
+                                      for (int j = 1; j <= slen; j++) {
+                                          if (dp[i - 1][j] || (i >= 2 && dp[i - 2][j])
+                                                  || (j >= 2 && dp[i][j - 1] && s[j - 1] == s[j - 2]
+                                                      && s[j - 2] == p[i - 2]))
+                                              dp[i][j] = true;
+                                      }
+                                  } else {
+                                      int j = 1;
+                                        // '.'首次匹配后的所有位置都能配上
+                                      while (!dp[i - 2][j] && j <= slen && !dp[i - 1][j])
+                                          j++;
+                                      for (; j <= slen; j++) {
+                                          dp[i][j] = true;
+                                      }
+                                  }
+                                  break;
+                              }
+                    default: {
+                                 for (int j = 1; j <= slen; j++) {
+                                     if (dp[i - 1][j - 1]
+                                             && (s[j - 1] == p[i - 1] || p[i - 1] == '.'))
+                                         //本列匹配，且i-1,j-1前都匹配
+                                         dp[i][j] = true;
+                                 }
+                                 break;
+                             }
+                }
+            }
+            return dp[plen][slen];
         }
 };
