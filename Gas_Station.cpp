93c1205eb80a5c91b2c8f2f479d006ce7bfaa22d
Kai Chen <chenkainp@gmail.com>
2013-10-01 14:59:14 +0800

    one more

---
+++ Gas_Station.cpp
@@ -0,0 +1,33 @@
+/*
+   interesting question, when sum<0, i go back
+   when sum>=0 j go forward, after n steps, check the sum, you will see
+   O(n)
+ */
+class Solution {
+    public:
+        int canCompleteCircuit(vector<int> &gas, vector<int> &cost) {
+            // Note: The Solution object is instantiated only once and is reused by each test case.
+            int sum=0,i=0,j=0,k=0,num=0;
+            int n=gas.size();
+            if(!n)return -1;
+
+            while(num<n)
+            {
+                sum+=(gas[k]-cost[k]);
+                if(sum<0)
+                {
+                    k=(i-1+n)%n;
+                    i=k;
+                }
+                else
+                {
+                    k=(j+1)%n;
+                    j=k;
+                }
+                num++;
+            }
+
+            if(sum>=0)return i;
+            else return -1;
+        }
+};
