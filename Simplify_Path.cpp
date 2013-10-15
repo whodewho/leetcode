a57849ca9e719f12dec2165eb83eb68b5504b84a
Kai Chen <chenkainp@gmail.com>
2013-10-15 10:23:51 +0800

    second try

--- Simplify_Path.cpp
+++ Simplify_Path.cpp
@@ -1,6 +1,8 @@
 /*
    details in mind, nothing special
    anson used deque, good structure.
+
+   second try
  */
 class Solution {
     public:
@@ -44,5 +46,45 @@ class Solution {
 
             string res(str.begin(), str.end());
             return res;
+        }
+
+
+        string simplifyPath(string path) {
+            // Note: The Solution object is instantiated only once and is reused by each test case.
+            vector<string> result;
+            for(int i=0;i<path.size();)
+            {
+                if(path[i]=='/')
+                {
+                    int j=i+1;
+                    while(j<path.size()&&path[j]!='/')j++;
+                    if(j==i+1)
+                    {
+                        i=j;
+                        continue;
+                    }
+
+                    string tmp=path.substr(i+1,j-i-1);
+                    i=j;
+                    if(tmp==".")continue;
+                    else if(tmp=="..")
+                    {
+                        if(result.empty())continue;
+                        else result.pop_back();
+                    }
+                    else result.push_back(tmp);
+                }
+                else
+                {
+                    i++;
+                }
+            }
+
+            string str="";
+            for(size_t i=0;i<result.size();i++)
+                str+=("/"+result[i]);
+            if(str=="")
+                return "/";
+            return str;
         }
 };
