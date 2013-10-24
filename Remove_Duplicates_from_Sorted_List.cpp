f9a465e3ae8d87a79453aa9264baa0ee0211673a
Kai Chen <chenkainp@gmail.com>
2013-10-24 21:00:51 +0800

    second try

--- Remove_Duplicates_from_Sorted_List.cpp
+++ Remove_Duplicates_from_Sorted_List.cpp
@@ -1,6 +1,8 @@
 /*
-    better
-   */
+   better
+
+   second try, as better
+ */
 class Solution {
     public:
         ListNode *deleteDuplicates(ListNode *head) {
@@ -15,5 +17,28 @@ class Solution {
                 else curNode = curNode->next;
             }
             return head;
+        }
+
+        ListNode *deleteDuplicates(ListNode *head) {
+            // Note: The Solution object is instantiated only once and is reused by each test case.
+            if(!head)return head;
+            ListNode* result=head;
+
+            while(head)
+            {
+                if(head->next)
+                {
+                    if(head->val==head->next->val)
+                    {
+                        ListNode* tmp=head->next;
+                        head->next=head->next->next;
+                        delete tmp;
+                        continue;
+                    }
+                }
+                head=head->next;
+            }
+
+            return result;
         }
 };
