5f5803f7c425b8004815f2362714a17539696adb
Kai Chen <chenkainp@gmail.com>
2013-10-25 21:24:18 +0800

    second try

--- Reverse_Nodes_in_k-Groups.cpp
+++ Reverse_Nodes_in_k-Groups.cpp
@@ -6,6 +6,10 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+
+/*
+   second try, seems better
+ */
 class Solution {
     public:
         ListNode *reverseKGroup(ListNode *head, int k) {
@@ -58,6 +62,44 @@ class Solution {
             }
 
             if(result==NULL)return head;
+            return result;
+        }
+
+        ListNode *reverseKGroup(ListNode *head, int k) {
+            // Note: The Solution object is instantiated only once and is reused by each test case.
+            if(!head)return head;
+            if(k==1)return head;
+
+            int num=0;
+            ListNode* runner=head;
+            while(runner)
+            {
+                num++;
+                runner=runner->next;
+            }
+            if(num<k)return head;
+
+            ListNode* result=NULL;
+            ListNode* tail=NULL;
+
+            for(int i=0;i<num/k;i++)
+            {
+                ListNode* newTail=head;
+                ListNode* newHead=head;
+                head=head->next;
+                for(int j=1;j<k;j++)
+                {
+                    ListNode* tmp=head->next;
+                    head->next=newHead;
+                    newHead=head;
+                    head=tmp;
+                }
+                if(!result)result=newHead;
+                if(tail)tail->next=newHead;
+                tail=newTail;
+                tail->next=head;
+            }
+
             return result;
         }
 };
