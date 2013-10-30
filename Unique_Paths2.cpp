d702a7dba1d169dcfd9de8a2d23511ca0cb46211
Kai Chen <chenkainp@gmail.com>
2013-10-30 14:59:39 +0800

    third try

--- Unique_Paths2.cpp
+++ Unique_Paths2.cpp
@@ -1,6 +1,8 @@
 /*
-    easy
-   */
+   easy
+
+   second try is awsome?
+ */
 class Solution {
     public:
         int uniquePathsWithObstacles(vector<vector<int> > &obstacleGrid) {
@@ -29,5 +31,25 @@ class Solution {
                     else p[i][j]=p[i+1][j]+p[i][j+1];
                 }
             return p[0][0];
+        }
+
+        int uniquePathsWithObstacles(vector<vector<int> > &obstacleGrid) {
+            // Note: The Solution object is instantiated only once and is reused by each test case.
+            int m=obstacleGrid.size();
+            if(!m)return 0;
+            int n=obstacleGrid[0].size();
+            if(!n)return 0;
+
+            vector<vector<int> > f(m,vector<int>(n,0));
+            for(int i=m-1;i>=0;i--)
+                for(int j=n-1;j>=0;j--)
+                {
+                    if(i==m-1&&j==n-1)f[i][j]=obstacleGrid[i][j]==1?0:1;
+                    else if(i==m-1)f[i][j]=obstacleGrid[i][j]==1?0:f[i][j+1];
+                    else if(j==n-1)f[i][j]=obstacleGrid[i][j]==1?0:f[i+1][j];
+                    else f[i][j]=obstacleGrid[i][j]==1?0:(f[i+1][j]+f[i][j+1]);
+                }
+
+            return f[0][0];
         }
 };
