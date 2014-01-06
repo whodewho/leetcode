82bb379a74e5b6fffe521e39376b1d534654726f
Kai Chen <chenkainp@gmail.com>
2014-01-06 13:44:51 +0800

    third try

---
+++ Binary_Tree_Postorder_Traversal.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+    public:
+        vector<int> postorderTraversal(TreeNode *root) {
+            vector<int> result;
+            if(!root) return result;
+            stack<TreeNode *> stk;
+            while(!stk.empty()||root)
+            {
+                while(root)
+                {
+                    stk.push(root);
+                    root=root->left;
+                }
+
+                if(!stk.empty()&&stk.top()->right&&(result.empty()||result.back()!=stk.top()->right->val))
+                {
+                    root=stk.top()->right;
+                }
+                else
+                {
+                    result.push_back(stk.top()->val);
+                    stk.pop();
+                    root=NULL;
+                }
+            }
+            return result;
+        }
+};
