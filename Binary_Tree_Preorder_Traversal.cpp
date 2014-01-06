9928d543df227905b5744c531c3f513dd0417b2e
Kai Chen <chenkainp@gmail.com>
2014-01-06 10:11:37 +0800

    third try

---
+++ Binary_Tree_Preorder_Traversal.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+    public:
+        vector<int> preorderTraversal(TreeNode *root) {
+            vector<int> result;
+            if(!root)return result;
+            stack<TreeNode*> stk;
+            while(!stk.empty()||root)
+            {
+                if(root)
+                {
+                    result.push_back(root->val);
+                    if(root->right)stk.push(root->right);
+                    root=root->left;
+                }
+                else
+                {
+                    root=stk.top();
+                    stk.pop();
+                }
+            }
+            return result;
+        }
+};
