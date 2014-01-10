6ce85955060e02b5ad4fe8a2391b8339cc32ab74
whodewho <chenkainp@gmail.com>
2014-01-10 23:00:24 +0800

    garfield

--- Container_With_Most_Water.cpp
+++ Container_With_Most_Water.cpp
@@ -1,22 +1,21 @@
 class Solution {
-    public:
-        int maxArea(vector<int> &height) {
-            int i = 0;
-            int j = height.size() - 1;
-            int max = 0;
-            while (i < j) {
-                int h = (height[i] < height[j]) ? height[i] : height[j];
-                int res = h * (j - i);
-                if (res > max) max = res;
-                if (height[i] <= height[j]) {
-                    i++;
-                    while (height[i] < h && i < j) i++;
-                }
-                else {
-                    j--;
-                    while (height[j] < h && i < j) j--;
-                }
-            }
-            return max;
-        }
+	public:
+		int maxArea(vector<int> &height) {
+			int i=0,j=height.size()-1;
+			int result=min(height[i],height[j])*(j-i);
+			while(i<j)
+			{
+				int h = min(height[i],height[j]);
+				if(height[i]<height[j])
+				{
+					while(height[i]<=h)i++;
+				}
+				else
+				{
+					while(height[j]<=h)j--;
+				}
+				result=max(min(height[i], height[j]) * (j-i), result);
+			}
+			return result;
+		}
 };
