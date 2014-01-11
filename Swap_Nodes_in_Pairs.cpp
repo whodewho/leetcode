e76df483797fded4c0d940a76479c78445e60c24
whodewho <chenkainp@gmail.com>
2014-01-11 17:26:14 +0800

    garfield

--- Swap_Nodes_in_Pairs.cpp
+++ Swap_Nodes_in_Pairs.cpp
@@ -7,44 +7,80 @@
  * };
  */
 class Solution {
-    public:
-        ListNode *swapPairs(ListNode *head) {
-            // Start typing your C/C++ solution below
-            // DO NOT write int main() function
-            ListNode* result=NULL;
-            ListNode* last=NULL;
-            while(head)
-            {
-                if(head->next)
-                {
-                    ListNode* tmp=head->next;
-                    head->next=tmp->next;
-                    tmp->next=head;
+	public:
+		ListNode *swapPairs(ListNode *head) {
+			// Start typing your C/C++ solution below
+			// DO NOT write int main() function
+			ListNode* result=NULL;
+			ListNode* last=NULL;
+			while(head)
+			{
+				if(head->next)
+				{
+					ListNode* tmp=head->next;
+					head->next=tmp->next;
+					tmp->next=head;
 
-                    if(result==NULL)
-                    {
-                        result=tmp;
-                        last=head;
-                    }
-                    else
-                    {
-                        last->next=tmp;
-                        last=head;
-                    }
-                }
-                else
-                {
-                    if(result==NULL)
-                    {
-                        result=head;
-                    }
-                    else
-                    {
-                        last->next=head;
-                    }
-                }
-                head=head->next;
-            }
-            return result;
-        }
+					if(result==NULL)
+					{
+						result=tmp;
+						last=head;
+					}
+					else
+					{
+						last->next=tmp;
+						last=head;
+					}
+				}
+				else
+				{
+					if(result==NULL)
+					{
+						result=head;
+					}
+					else
+					{
+						last->next=head;
+					}
+				}
+				head=head->next;
+			}
+			return result;
+		}
+
+		ListNode *swapPairs(ListNode *head) {
+			ListNode* result=NULL;
+			ListNode *runner=NULL;
+
+			while(head)
+			{
+				ListNode* first=head;
+				ListNode* second=head->next;
+				if(second==NULL)
+				{
+					if(runner)runner->next=head;
+					else result=head;
+					break;
+				}
+				ListNode* third=second->next;
+
+				if(result==NULL)
+				{
+					result=second;
+					second->next=first;
+					first->next=NULL;
+					runner=first;
+				}
+				else
+				{
+					runner->next=second;
+					second->next=first;
+					first->next=NULL;
+					runner=first;
+				}
+
+				head=third;
+			}
+			return result;
+		}
 };
