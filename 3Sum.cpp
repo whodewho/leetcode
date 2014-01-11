220359bb394998c230130b95803d0ffa83516ed6
whodewho <chenkainp@gmail.com>
2014-01-11 10:28:58 +0800

    garfield

--- 3Sum.cpp
+++ 3Sum.cpp
@@ -1,34 +1,44 @@
 /*
-    sort, than dfs will work
-    this is anson's, better
+   sort, than dfs will work
+   this is anson's, better
+
+   the input become harder, revise one
    */
+
+
 class Solution {
-    public:
-        vector<vector<int> > threeSum(vector<int> &num) {
-            int N = num.size();
-            sort(num.begin(), num.end());
-            vector<vector<int> > result;
-            for (int k = 0; k < N-2; k++) {
-                int i = k+1;
-                int j = N-1;
-                while (i < j) {
-                    int sum = num[i] + num[j] + num[k];
-                    if (sum > 0) j--;
-                    else if (sum < 0) i++;
-                    else {
-                        vector<int> triplet;
-                        triplet.push_back(num[i]);
-                        triplet.push_back(num[j]);
-                        triplet.push_back(num[k]);
-                        sort(triplet.begin(), triplet.end());
-                        if (find(result.begin(), result.end(), triplet) == result.end()) {
-                            result.push_back(triplet);
-                        }
-                        i++;
-                        j--;
-                    }
-                }
-            }
-            return result;
-        }
+	public:
+		vector<vector<int> > threeSum(vector<int> &num) {
+			// Start typing your C/C++ solution below
+			// DO NOT write int main() function
+			vector<vector<int> > result;
+			int m=num.size();
+			if(!m)return result;
+
+			sort(num.begin(),num.end());
+			vector<int> last;
+			for(int i=0;i<m-2;i++)
+			{
+				if (i>0 && num[i]==num[i-1]) continue; 
+				int j=i+1,k=m-1;
+				while(j<k)
+				{
+					int tmpSum=num[i]+num[j]+num[k];
+					if(tmpSum>0)k--;
+					else if(tmpSum<0)j++;
+					else{
+						vector<int> tmpVec;
+						tmpVec.push_back(num[i]);
+						tmpVec.push_back(num[j]);
+						tmpVec.push_back(num[k]);
+
+						result.push_back(tmpVec);
+
+						do{j++;}while(k>j&&num[j]==num[j-1]);
+						do{k--;}while(k>j&&num[k]==num[k+1]);
+					}
+				}
+			}
+			return result;
+		}
 };
