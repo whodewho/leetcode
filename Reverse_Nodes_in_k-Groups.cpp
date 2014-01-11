bb9b0e1927de2a920a6c0dc1b4841795c8c1d855
whodewho <chenkainp@gmail.com>
2014-01-11 19:42:48 +0800

    garfield

--- Reverse_Nodes_in_k-Groups.cpp
+++ Reverse_Nodes_in_k-Groups.cpp
@@ -9,97 +9,165 @@
 
 /*
    second try, seems better
- */
+   garfield try, same with first try
+   follow instinction
+   I can do it!
+   */
 class Solution {
-    public:
-        ListNode *reverseKGroup(ListNode *head, int k) {
-            // Start typing your C/C++ solution below
-            // DO NOT write int main() function
-            ListNode* result=NULL;
-            ListNode* last=NULL;
-            ListNode* start=head;
-            while(start)
-            {
-                int i=0;
-                ListNode* end=start;
-                while(i<k&&end)
-                {
-                    end=end->next;
-                    i++;
-                }
+	public:
+		ListNode *reverseKGroup(ListNode *head, int k) {
+			// Start typing your C/C++ solution below
+			// DO NOT write int main() function
+			ListNode* result=NULL;
+			ListNode* last=NULL;
+			ListNode* start=head;
+			while(start)
+			{
+				int i=0;
+				ListNode* end=start;
+				while(i<k&&end)
+				{
+					end=end->next;
+					i++;
+				}
 
-                if(i<k)break;
+				if(i<k)break;
 
-                i=0;
-                ListNode* tmpStart=NULL;
-                ListNode* tmpEnd=NULL;
-                while(i<k)
-                {
-                    if(tmpStart==NULL)
-                    {
-                        tmpStart=start;
-                        tmpEnd=start;
-                        start=start->next;
-                    }
-                    else
-                    {
-                        ListNode* tmp=start->next;
-                        start->next=tmpStart;
-                        tmpStart=start;
-                        start=tmp;
-                    }
-                    i++;
-                }
+				i=0;
+				ListNode* tmpStart=NULL;
+				ListNode* tmpEnd=NULL;
+				while(i<k)
+				{
+					if(tmpStart==NULL)
+					{
+						tmpStart=start;
+						tmpEnd=start;
+						start=start->next;
+					}
+					else
+					{
+						ListNode* tmp=start->next;
+						start->next=tmpStart;
+						tmpStart=start;
+						start=tmp;
+					}
+					i++;
+				}
 
-                if(result==NULL)result=tmpStart;
-                if(last==NULL)last=tmpEnd;
-                else
-                {
-                    last->next=tmpStart;
-                    last=tmpEnd;
-                }
-                last->next = start;
-            }
+				if(result==NULL)result=tmpStart;
+				if(last==NULL)last=tmpEnd;
+				else
+				{
+					last->next=tmpStart;
+					last=tmpEnd;
+				}
+				last->next = start;
+			}
 
-            if(result==NULL)return head;
-            return result;
-        }
+			if(result==NULL)return head;
+			return result;
+		}
 
-        ListNode *reverseKGroup(ListNode *head, int k) {
-            // Note: The Solution object is instantiated only once and is reused by each test case.
-            if(!head)return head;
-            if(k==1)return head;
+		ListNode *reverseKGroup(ListNode *head, int k) {
+			// Note: The Solution object is instantiated only once and is reused by each test case.
+			if(!head)return head;
+			if(k==1)return head;
 
-            int num=0;
-            ListNode* runner=head;
-            while(runner)
-            {
-                num++;
-                runner=runner->next;
-            }
-            if(num<k)return head;
+			int num=0;
+			ListNode* runner=head;
+			while(runner)
+			{
+				num++;
+				runner=runner->next;
+			}
+			if(num<k)return head;
 
-            ListNode* result=NULL;
-            ListNode* tail=NULL;
+			ListNode* result=NULL;
+			ListNode* tail=NULL;
 
-            for(int i=0;i<num/k;i++)
-            {
-                ListNode* newTail=head;
-                ListNode* newHead=head;
-                head=head->next;
-                for(int j=1;j<k;j++)
-                {
-                    ListNode* tmp=head->next;
-                    head->next=newHead;
-                    newHead=head;
-                    head=tmp;
-                }
-                if(!result)result=newHead;
-                if(tail)tail->next=newHead;
-                tail=newTail;
-                tail->next=head;
-            }
+			for(int i=0;i<num/k;i++)
+			{
+				ListNode* newTail=head;
+				ListNode* newHead=head;
+				head=head->next;
+				for(int j=1;j<k;j++)
+				{
+					ListNode* tmp=head->next;
+					head->next=newHead;
+					newHead=head;
+					head=tmp;
+				}
+				if(!result)result=newHead;
+				if(tail)tail->next=newHead;
+				tail=newTail;
+				tail->next=head;
+			}
 
-            return result;
-        }
+			return result;
+		}
+
+		ListNode *reverseKGroup(ListNode *head, int k) {
+			if(k==1)return head;
+			if(!head)return head;
+			ListNode* result=NULL;
+			ListNode* tail=NULL;
+
+			while(true)
+			{
+				int i=0;
+				ListNode* runner=head;
+				while(runner)
+				{
+					i++;
+					runner=runner->next;
+				}
+
+				if(i<k)
+				{
+					if(!result)return head;
+					else
+					{
+						tail->next=head;
+						return result;
+					}
+				}
+				else
+				{
+					runner=head;
+					ListNode* tmpHead=NULL;
+					ListNode* tmpTail=NULL;
+					for(int i=0;i<k;i++)
+					{
+						if(!tmpHead)
+						{
+							tmpHead=head;
+							tmpTail=head;
+							runner=runner->next;
+						}
+						else
+						{
+							ListNode* tmp=runner->next;
+							runner->next=tmpHead;
+							tmpHead=runner;
+							runner=tmp;
+						}
+					}
+
+					head=runner;
+					tmpTail->next=NULL;
+					if(!result)
+					{
+						result=tmpHead;
+						tail=tmpTail;
+					}
+					else
+					{
+						tail->next=tmpHead;
+						tail=tmpTail;
+					}
+				}
+			}
+
+			return result;
+		}
 };
